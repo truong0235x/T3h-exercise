e04e60d61ee646b6d1665eb7df32010826156359
truong0235x <maxphe0235@gmail.com>
2020-12-23 19:55:04 +0700

    Update homework/unit-13

--- unit-13/javascrip-even/main.js
+++ unit-13/javascrip-even/main.js
@@ -1,4 +1,5 @@
-// lesson 1
+// exercise 1
+function getCurrent () {
 const date = new Date()
 const day = date.getDate()
 const month = date.getMonth() + 1
@@ -8,40 +9,55 @@ const minutes = date.getMinutes()
 const seconds = date.getSeconds()
 const currentTime = `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`
 
-document.getElementById('onmouse').onmouseover = function () {
-	console.log(`Hover lúc: ${currentTime}`)
+return currentTime
 }
-document.getElementById('onmouse').onmouseout = function () {
-	console.log(`Out hover lúc: ${currentTime}`)
+
+const onHoverDiv = event => {
+  const currentTime = getCurrent()
+  console.log(`Hover lúc: ${currentTime}`)
 }
-// lesson 2
-document.getElementById('input').oninput = function () {
-	const inputValue = document.getElementById('input').value
-	const regex = /^[a-z0-9_]{0,}[^A-Z\.,!@#$%^&*()~+-][a-z0-9_]{0,}$/
-	if (!(regex.test(inputValue))) {
-		alert('Date a-z (lowercase), and the numbers 0-9, and an underscore')
-	}
+
+const onNotHoverDiv = event => {
+  const currentTime = getCurrent()
+  console.log(`Out hover lúc: ${currentTime}`)
 }
-// lesson 3
-document.getElementById('inputnumber').oninput = function () {
-	const inputNumber = document.getElementById('inputnumber').value
-	console.log(inputNumber)
-	const regexNumber = /^1[^1-9]{0,3}$|^[0][0-9]{0,3}$/
-	if (!(regexNumber.test(inputNumber))) {
-		alert('Date numbers 0-9, and The maximum input amount is 1000')
-	}
+
+// exercise 2
+const checkAccount = event => {
+  const value = event.target.value
+  const regex = /^[a-z0-9_]+$/g
+  const match = regex.test(value)
+
+  if(!match) {
+    alert('Account chỉ được phép chứa các ký tự sau...')
+  }
 }
-// lesson 4
-document.getElementById('doubleclick').ondblclick = function () {
-	alert('Double click')
+// exercise 3
+function checkAmount(event) {
+    const value = event.target.value
+    const regex = /^[0-9\.-]+$/g
+    const match = regex.test(value)
+
+    if (!regex.test(value)) {
+        event.target.value = value.replace(/[^0-9\.]/g, '')
+    }
+
+    value = event.target.value
+    if (Number(value) > 1000) {
+        console.log('Max Amount 1000')
+    }
 }
-// lesson 5
-document.getElementById('onselect').onselect = function () {
-	const values = document.getElementById('onselect').value
-	alert(`${values}`)
+// Example 4
+const checkDoubleClick = () => alert('Double click')
+
+// Example 5
+const getText = () => {
+  let text = "";
+  text = window.getSelection()
+  console.log(text.toString())
 }
-// leeson 6
-document.getElementById('select').onchange = function () {
-	const value = document.getElementById('select').value
-	console.log(value)
+
+// Example 6
+const getFluits = event => {
+  console.log(event.target.options[event.target.selectedIndex].text)
 }
